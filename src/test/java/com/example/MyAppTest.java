140b049774b224f8f8303777323e7d3582581981
jaydeep123s <132106205+jaydeep123s@users.noreply.github.com>
2024-08-13 20:53:24 +0530

    Create MyAppTest.java

---
+++ src/test/java/com/example/MyAppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyAppTest {
+
+    @Test
+    public void testMain() {
+        MyApp app = new MyApp();
+        assertEquals(1, 1); // simple test
+    }
+}
